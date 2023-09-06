7d43a498c2fc9b7038e700a418d2a0870b26fd97
Chirag Kukreja <chirag.kukreja24@gmail.com>
2023-09-06 10:02:17 +0530

    Printing Red Orange and Green Messages

--- 4_echoing_message_with_green_red.sh
+++ 4_echoing_message_with_green_red.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#This script print the message in green and red
+#This script print the message in green and red and orange
 echo -e "\033[0;31m your failed message"
 echo -e "\033[0;32m your success message"
+echo -e "\033[0;33m your warning message"
