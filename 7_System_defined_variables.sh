c573e91b820b4ad06dc0f34fa1819affa8eb957c
Chirag Kukreja <chirag.kukreja24@gmail.com>
2023-09-10 18:06:42 +0530

    System Defined Var

---
+++ 7_System_defined_variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script shows the system defined variables
+
+echo ${SHELL}
+echo ${HOME}
+echo ${OSTYPE}
+echo ${PATH}
+echo ${$} #Process ID
+echo ${PPID} #Parent process id
+echo ${PWD}
+echo ${HOSTNAME}
+echo ${UID}
+sleep 5
+echo ${SECONDS}
