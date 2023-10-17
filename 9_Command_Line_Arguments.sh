a9adc7a55cd8083c21df793e2361fe24ef7f5c1c
Chirag Kukreja <chirag.kukreja24@gmail.com>
2023-10-17 10:00:09 +0530

    Command Line Arguments

--- 9_Command_Line_Arguments.sh
+++ 9_Command_Line_Arguments.sh
@@ -8,3 +8,7 @@ echo ${2}
 name=${1}
 age=${2}
 echo "my name is ${name}, and my age is ${age}"
+
+echo $# #It tells how many command line arguments are passed 
+echo $@ #It prints all the CLI inputted. 
+echo $* #It prints all the CLI inputted as a string
