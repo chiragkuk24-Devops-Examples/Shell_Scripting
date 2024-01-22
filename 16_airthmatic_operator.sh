babe4b60715f16c16d5560e826731b2efb4e4b1c
Chirag Kukreja <chirag.kukreja24@gmail.com>
2024-01-22 21:19:19 +0530

    Airthmatic operations

---
+++ 16_airthmatic_operator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+a=5
+b=6
+echo "$((a+b))"
+echo "$((a-b))"
+echo "$((a*b))"
+echo "$((b/a))"
+echo "$((a%b))"
+echo "$((2**3))" # 2*2*2
+ ((a++)) #a=a+1
+ echo $a
+
+ ((a+=3)) #a=a+3
+ echo $a
